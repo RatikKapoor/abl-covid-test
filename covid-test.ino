6ea121963cfee5db4a7d7d0da18ec917f30813f2
Ratik Kapoor <ratikrk@gmail.com>
2020-07-02 00:03:59 -0600

    Control three status' using REST API

--- covid-test.ino
+++ covid-test.ino
@@ -17,7 +17,11 @@
 #include <XTronical_ST7735.h> // Hardware-specific library
 #include <SPI.h>
 
-#define LED_BUILTIN 2   // Set the GPIO pin where you connected your test LED or comment this line out if your dev board has a built-in LED
+//#define LED_BUILTIN 2   // Set the GPIO pin where you connected your test LED or comment this line out if your dev board has a built-in LED
+#define LED1 32
+#define LED2 33
+#define LED3 25
+
 
 // Wifi AP Credentials
 const char *ssid = "ESP32_ABL_NET";
@@ -29,10 +33,16 @@ const char *password = "password";
 #define TFT_CS 5
 
 WiFiServer server(80);
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);  
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
+
+bool oneState = false;
+bool twoState = false;
+bool threeState = false;
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
   tft.init();
   tft.setRotation(3);
   tft.fillScreen(ST7735_BLACK);
@@ -48,10 +58,12 @@ void setup() {
   Serial.println(myIP);
   server.begin();
 
-  digitalWrite(LED_BUILTIN, LOW);
+  pinOutput(LED1, LOW);
+  pinOutput(LED2, LOW);
+  pinOutput(LED3, LOW);
 
   Serial.println("Server started");
-  text("Server started\nLED Off");
+  text("Server started, LEDs Off");
 }
 
 void loop() {
@@ -72,12 +84,14 @@ void loop() {
             // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
             // and a content-type so the client knows what's coming, then a blank line:
             client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:application/json");
+            client.println("Content-type:text/html");
             client.println();
 
             // the content of the HTTP response follows the header:
-            client.print("SUCCCESS");
-            // client.print("Click <a href=\"/L\">here</a> to turn OFF the LED.<br>");
+//            client.print("SUCCCESS");
+           client.print("Change LED1 state: <a href=\"/H1\">On</a> or <a href=\"/L1\">Off</a><br>");
+           client.print("Change LED2 state: <a href=\"/H2\">On</a> or <a href=\"/L2\">Off</a><br>");
+           client.print("Change LED3 state: <a href=\"/H3\">On</a> or <a href=\"/L3\">Off</a><br>");
 
             // The HTTP response ends with another blank line:
             client.println();
@@ -91,13 +105,29 @@ void loop() {
         }
 
         // Check to see if the client request was "GET /H" or "GET /L":
-        if (currentLine.endsWith("GET /H")) {
-          digitalWrite(LED_BUILTIN, HIGH);
-          text("Server started\nLED On");
+        if (currentLine.endsWith("GET /H1")) {
+          pinOutput(LED1, HIGH);
+          tftStatus();
         }
-        if (currentLine.endsWith("GET /L")) {
-          digitalWrite(LED_BUILTIN, LOW);
-          text("Server started\nLED Off");
+        if (currentLine.endsWith("GET /L1")) {
+          pinOutput(LED1, LOW);
+          tftStatus();
+        }
+        if (currentLine.endsWith("GET /H2")) {
+          pinOutput(LED2, HIGH);
+          tftStatus();
+        }
+        if (currentLine.endsWith("GET /L2")) {
+          pinOutput(LED2, LOW);
+          tftStatus();
+        }
+        if (currentLine.endsWith("GET /H3")) {
+          pinOutput(LED3, HIGH);
+          tftStatus();
+        }
+        if (currentLine.endsWith("GET /L3")) {
+          pinOutput(LED3, LOW);
+          tftStatus();
         }
       }
     }
@@ -113,4 +143,41 @@ void text(char *text) {
   tft.setTextColor(0xFFFF);
   tft.setTextWrap(true);
   tft.print(text);
+}
+
+void text(const char *text, int x, int y) {
+//  tft.fillScreen(ST7735_BLACK);
+  tft.setCursor(x,y);
+  tft.setTextColor(0xFFFF);
+  tft.setTextWrap(true);
+  tft.print(text);
+}
+
+void pinOutput(int pin, bool state) {
+  if (state) {
+    digitalWrite(pin, HIGH);
+  } else {
+    digitalWrite(pin, LOW);
+  }
+  switch (pin) {
+    case LED1:
+      oneState = state;
+      break;
+    case LED2:
+      twoState = state;
+      break;
+    case LED3:
+      threeState = state;
+      break;
+    default:
+      // Default
+      break;
+  }
+}
+
+void tftStatus() {
+  text("Server runnning");
+  text(oneState ? "LED1: On" : "LED1: Off", 5, 20);
+  text(twoState ? "LED2: On" : "LED2: Off", 5, 30);
+  text(threeState ? "LED3: On" : "LED3: Off", 5, 40);
 }
