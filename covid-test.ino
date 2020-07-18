f85628a1a218c2affd7403d878188e31a362b2a8
Ratik Kapoor <ratikrk@gmail.com>
2020-07-17 22:36:18 -0600

    Introduce class-based code organization

--- covid-test.ino
+++ covid-test.ino
@@ -7,187 +7,232 @@
  * 
  */
 
+// Include servo software
+#include <ESP32_Servo.h>
+
+// Include Adafruit Graphics Library (to be used with ST7735 display)
+#include <Adafruit_GFX.h>     // Core graphics library
+#include <XTronical_ST7735.h> // Hardware-specific library
+#include <SPI.h>
+
 // Include Wifi module drivers
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WiFiAP.h>
 
-// Include Adafruit Graphics Library (to be used with ST7735 display)
-#include <Adafruit_GFX.h>    // Core graphics library
-#include <XTronical_ST7735.h> // Hardware-specific library
-#include <SPI.h>
-
-//#define LED_BUILTIN 2   // Set the GPIO pin where you connected your test LED or comment this line out if your dev board has a built-in LED
-#define LED1 32
-#define LED2 33
-//#define LED3 25
-
-// Motor
-#include <ESP32_Servo.h>
-int servoPin = 25;
-Servo servo;
-
-
-// Wifi AP Credentials
-const char *ssid = "ESP32_ABL_NET";
-const char *password = "password";
-
-// LCD GPIO Pins
+// Define pin designations
+#define HEATER_PIN 32
+#define LED_ARRAY_PIN 33
+#define SERVO_PIN 25
 #define TFT_DC 2
 #define TFT_RST -1
 #define TFT_CS 5
 
-WiFiServer server(80);
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
+/**
+ * 
+ * MotorController
+ * 
+ */
+class MotorController
+{
+private:
+    Servo servo;
+    int power = 0;
+    int ms = 1500;
+    int pin;
 
-bool oneState = false;
-bool twoState = false;
-bool threeState = false;
+public:
+    MotorController(int pin);
+    ~MotorController();
+    void setPower(int power);
+    int getPower();
+};
 
-void setup() {
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-//  pinMode(LED3, OUTPUT);
-  tft.init();
-  tft.setRotation(3);
-  tft.fillScreen(ST7735_BLACK);
-
-  Serial.begin(115200);
-  Serial.println();
-  text("Configuring access point...");
-  Serial.println("Configuring access point...");
-
-  WiFi.softAP(ssid, password);
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(myIP);
-  server.begin();
-
-  pinOutput(LED1, LOW);
-  pinOutput(LED2, LOW);
-//  pinOutput(LED3, LOW);
-
-  Serial.println("Server started");
-  text("Server started, LEDs Off");
-  servo.attach(servoPin);
-  servo.writeMicroseconds(1500); // send "stop" signal to ESC.
-  delay(3000);
+MotorController::MotorController(int pin)
+{
+    this->pin = pin;
+    servo.attach(pin);
+    servo.writeMicroseconds(ms);
 }
 
-void loop() {
-  int signal = 1700;
-  servo.writeMicroseconds(signal);
-  WiFiClient client = server.available();   // listen for incoming clients
-
-  if (client) {                             // if you get a client,
-    Serial.println("New Client.");           // print a message out the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        if (c == '\n') {                    // if the byte is a newline character
-
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
-
-            // the content of the HTTP response follows the header:
-//            client.print("SUCCCESS");
-           client.print("Change LED1 state: <a href=\"/H1\">On</a> or <a href=\"/L1\">Off</a><br>");
-           client.print("Change LED2 state: <a href=\"/H2\">On</a> or <a href=\"/L2\">Off</a><br>");
-           client.print("Change LED3 state: <a href=\"/H3\">On</a> or <a href=\"/L3\">Off</a><br>");
-
-            // The HTTP response ends with another blank line:
-            client.println();
-            // break out of the while loop:
-            break;
-          } else {    // if you got a newline, then clear currentLine:
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-
-        // Check to see if the client request was "GET /H" or "GET /L":
-        if (currentLine.endsWith("GET /H1")) {
-          pinOutput(LED1, HIGH);
-          tftStatus();
-        }
-        if (currentLine.endsWith("GET /L1")) {
-          pinOutput(LED1, LOW);
-          tftStatus();
-        }
-        if (currentLine.endsWith("GET /H2")) {
-          pinOutput(LED2, HIGH);
-          tftStatus();
-        }
-        if (currentLine.endsWith("GET /L2")) {
-          pinOutput(LED2, LOW);
-          tftStatus();
-        }
-        if (currentLine.endsWith("GET /H3")) {
-//          pinOutput(LED3, HIGH);
-          tftStatus();
-        }
-        if (currentLine.endsWith("GET /L3")) {
-//          pinOutput(LED3, LOW);
-          tftStatus();
-        }
-      }
-    }
-    // close the connection:
-    client.stop();
-    Serial.println("Client Disconnected.");
-  }
+MotorController::~MotorController()
+{
 }
 
-void text(char *text) {
-  tft.fillScreen(ST7735_BLACK);
-  tft.setCursor(5,5);
-  tft.setTextColor(0xFFFF);
-  tft.setTextWrap(true);
-  tft.print(text);
+int MotorController::getPower()
+{
+    return power;
 }
 
-void text(const char *text, int x, int y) {
-//  tft.fillScreen(ST7735_BLACK);
-  tft.setCursor(x,y);
-  tft.setTextColor(0xFFFF);
-  tft.setTextWrap(true);
-  tft.print(text);
+void MotorController::setPower(int power)
+{
+    ms = map(power, 0, 100, 1500, 1700);
 }
 
-void pinOutput(int pin, bool state) {
-  if (state) {
-    digitalWrite(pin, HIGH);
-  } else {
-    digitalWrite(pin, LOW);
-  }
-  switch (pin) {
-    case LED1:
-      oneState = state;
-      break;
-    case LED2:
-      twoState = state;
-      break;
-//    case LED3:
-//      threeState = state;
-//      break;
-    default:
-      // Default
-      break;
-  }
+/**
+ * 
+ * RelayController
+ * 
+ */
+class RelayController
+{
+private:
+    int pin;
+    bool state = false;
+public:
+    RelayController(int pin);
+    ~RelayController();
+    void setState(bool state);
+    bool getState();
+};
+
+RelayController::RelayController(int pin)
+{
+    this->pin = pin;
 }
 
-void tftStatus() {
-  text("Server runnning");
-  text(oneState ? "LED1: On" : "LED1: Off", 5, 20);
-  text(twoState ? "LED2: On" : "LED2: Off", 5, 30);
-  text(threeState ? "LED3: On" : "LED3: Off", 5, 40);
+RelayController::~RelayController()
+{
+}
+
+bool RelayController::getState()
+{
+    return this->state;
+}
+
+void RelayController::setState(bool state)
+{
+    this->state = state;
+}
+
+
+/**
+ * 
+ * IO
+ * 
+ */
+class IO
+{
+private:
+    MotorController *motorController;
+    RelayController *heaterController;
+    RelayController *ledArrayController;
+
+public:
+    IO(int motorPin, int heaterPin, int ledArrayPin);
+    ~IO();
+    void setMotorPower(int power);
+    int getMotorPower();
+    void setHeaterState(bool state);
+    bool getHeaterState();
+    void setLedArrayState(bool state);
+    bool getLedArrayState();
+};
+
+IO::IO(int motorPin, int heaterPin, int ledArrayPin)
+{
+    this->motorController =  new MotorController(motorPin);
+    this->heaterController =  new RelayController(heaterPin);
+    this->ledArrayController =  new RelayController(ledArrayPin);
+}
+
+IO::~IO()
+{
+}
+
+void IO::setMotorPower(int power)
+{
+    motorController->setPower(power);
+    return;
+}
+
+int IO::getMotorPower()
+{
+    return motorController->getPower();
+}
+
+void IO::setHeaterState(bool state)
+{
+    heaterController->setState(state);
+    return;
+}
+
+bool IO::getHeaterState()
+{
+    return heaterController->getState();
+}
+
+void IO::setLedArrayState(bool state)
+{
+    ledArrayController->setState(state);
+    return;
+}
+
+bool IO::getLedArrayState()
+{
+    return ledArrayController->getState();
+}
+
+/**
+ * 
+ * TFT
+ * 
+ */
+class TFT
+{
+private:
+    /* data */
+public:
+    TFT(/* args */);
+    ~TFT();
+};
+
+TFT::TFT(/* args */)
+{
+}
+
+TFT::~TFT()
+{
+}
+
+/**
+ * 
+ * WifiAP
+ * 
+ */
+class WifiAP
+{
+private:
+    WifiServer server(80);
+public:
+    WifiAP(string ssid, string password);
+    ~WifiAP();
+};
+
+WifiAP::WifiAP(string ssid, string password)
+{
+    WiFi.softAP(ssid, password);
+    server.begin();
+}
+
+WifiAP::~WifiAP()
+{
+}
+
+/**
+ * 
+ * Setup
+ * 
+ */
+void setup()
+{
+}
+
+/**
+ * 
+ * Loop
+ * 
+ */
+void loop()
+{
 }
